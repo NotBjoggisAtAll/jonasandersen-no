a1f12af6ce1f6101ab4d97af55b246116a1be5aa
Jonas Bjørge Andersen <dev@jonasandersen.no>
2023-12-16 12:37:56 +0100

    Initial commit

---
+++ src/main/java/com/bjoggis/jonasandersen/JonasandersenApplication.java
@@ -0,0 +1,13 @@
+package com.bjoggis.jonasandersen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JonasandersenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JonasandersenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bjoggis/jonasandersen/JonasandersenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjoggis.jonasandersen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JonasandersenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
