1417fb0f96c09bfb5a8ce5eca7548daed355fddd
Jonas Bjørge Andersen <jonas.bjorge.andersen@gmail.com>
2024-07-15 19:50:46 +0200

    Update spring boot java and how to build

--- src/main/java/com/bjoggis/jonasandersen/IndexController.java
+++ src/main/java/com/bjoggis/jonasandersen/IndexController.java
@@ -1,15 +1,13 @@
 package com.bjoggis.jonasandersen;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
   @GetMapping("/")
-  public String index(Model model) {
-
+  public String index() {
     return "index";
   }
 }
